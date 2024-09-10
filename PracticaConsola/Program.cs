efa197dc33685e21805c98277cafd55b7fdbfb76
Esteban Martínez <Estebancertificates@gmail.com>
2024-09-11 00:50:31 +0200

    practica de consola

---
+++ PracticaConsola/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("nombres : ");
+string nom = Console.ReadLine();
+Console.Write("apellidos : ");
+string ape = Console.ReadLine();
+Console.Write("edad  : ");
+int edad = int.Parse(Console.ReadLine());
+Console.Write("hobby : ");
+string hob = Console.ReadLine();
+Console.WriteLine("==================INFORMACION BÁSICA=================");
+Console.WriteLine($"eres {nom} {ape}.");
+Console.WriteLine($"tienes {edad} años.");
+Console.WriteLine($"y tu hobby es {hob}");
+Console.Write("====================================================");
