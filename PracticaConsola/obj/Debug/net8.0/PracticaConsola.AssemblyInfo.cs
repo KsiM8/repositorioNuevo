ce8e59a5d689fd8ff1535853f31d8fc487369568
Esteban Martínez <Estebancertificates@gmail.com>
2024-09-12 20:54:30 +0200

    cambio de titulo

--- PracticaConsola/obj/Debug/net8.0/PracticaConsola.AssemblyInfo.cs
+++ PracticaConsola/obj/Debug/net8.0/PracticaConsola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticaConsola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa197dc33685e21805c98277cafd55b7fdbfb76")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticaConsola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticaConsola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
